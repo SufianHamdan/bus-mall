d70196195f30befd9a43fb773b1d119d89835205
SufianHamdan <sufian_hamdan@yahoo.com>
2021-03-29 21:25:41 +0300

    lab12-solved

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 
 const imageNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 
+const imageNamesForChart = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
 
 let leftInd;
 let middleInd;
@@ -11,9 +13,11 @@ let rightInd;
 let roundsOfVoting = 5;
 let counter = 0;
 
+let votesArr = [];
+let viewsArr = [];
 
-function Product (name)
-{
+
+function Product(name) {
   this.name = name;
   this.path = `./images/${name}`;
   this.votes = 0;
@@ -23,8 +27,7 @@ function Product (name)
 Product.allProducts = [];
 
 
-for(let i = 0; i < imageNames.length; i++)
-{
+for (let i = 0; i < imageNames.length; i++) {
   new Product(imageNames[i]);
 }
 
@@ -41,8 +44,7 @@ const imagesSection = document.getElementById('images-section');
 displayImagesForVotes();
 
 
-function displayImagesForVotes()
-{
+function displayImagesForVotes() {
   getUniqueNumb();
 
   leftImage.src = Product.allProducts[leftInd].path;
@@ -64,32 +66,23 @@ function displayImagesForVotes()
 
 
 imagesSection.addEventListener('click', userInteraction);
-function userInteraction(event)
-{
-  if(event.target.id !== 'images-section')
-  {
-    if(event.target.id === rightImage.id)
-    {
+function userInteraction(event) {
+  if (event.target.id !== 'images-section') {
+    if (event.target.id === rightImage.id) {
+
       Product.allProducts[rightInd].votes++;
-      displayImagesForVotes();
-      counter++;
     }
-    else if(event.target.id === middleImage.id)
-    {
+    else if (event.target.id === middleImage.id) {
       Product.allProducts[middleInd].votes++;
-      displayImagesForVotes();
-      counter++;
     }
-    else
-    {
+    else {
       Product.allProducts[rightInd].votes++;
-      displayImagesForVotes();
-      counter++;
     }
+    displayImagesForVotes();
+    counter++;
   }
   // console.table(Product.allProducts);
-  if ( counter >= roundsOfVoting)
-  {
+  if (counter >= roundsOfVoting) {
     imagesSection.removeEventListener('click', userInteraction);
 
     const btn = document.createElement('button');
@@ -101,49 +94,82 @@ function userInteraction(event)
 
 }
 
-function votingResult()
-{
+function votingResult() {
   const listParent = document.createElement('ul');
   imagesSection.appendChild(listParent);
 
-  for(let i = 0; i < Product.allProducts.length; i++)
-  {
+  for (let i = 0; i < Product.allProducts.length; i++) {
     const listItem = document.createElement('li');
     listParent.appendChild(listItem);
     let name = removeImageExt((String)(Product.allProducts[i].name));
-    listItem.textContent = name +' had '+Product.allProducts[i].votes + ' votes, and was seen '+Product.allProducts[i].views+' times.';
+    listItem.textContent = name + ' had ' + Product.allProducts[i].votes + ' votes, and was seen ' + Product.allProducts[i].views + ' times.';
+
+    votesArr.push(Product.allProducts[i].votes);
+    viewsArr.push(Product.allProducts[i].views);
   }
+  chartDrow();
 }
 
-function removeImageExt(name){
+function removeImageExt(name) {
   return name.substring(0, name.indexOf('.'));
 }
 
-function getUniqueNumb()
-{
-  let numbers = [];
 
+
+function getUniqueNumb() {
+  let numbers = [];
   let min, max, r, n, p;
 
   min = 0;
   max = imageNames.length - 1;
-  r = 3;
+  r = 6;
 
   for (let i = 0; i < r; i++) {
     do {
       n = Math.floor(Math.random() * (max - min + 1)) + min;
       p = numbers.includes(n);
-      if(!p){
+      if (!p) {
         numbers.push(Number(n));
       }
     }
-    while(p);
+    while (p);
   }
-  for(let i = 0; i < numbers.length; i++)
-  {
-    leftInd = numbers.pop();
-    middleInd = numbers.pop();
-    rightInd = numbers.pop();
+  for (let i = 0; i < numbers.length; i++) {
+    leftInd = numbers[i];
+    i++;
+    middleInd = numbers[i];
+    i++;
+    rightInd = numbers[i];
   }
 }
+
+function chartDrow() {
+  console.log(votesArr);
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+      labels: imageNamesForChart,
+      datasets: [{
+        label: 'Votes',
+        backgroundColor: '#041412',
+        borderColor: 'rgb(255, 99, 10)',
+        data: votesArr
+      },
+      {
+        label: 'Views',
+        backgroundColor: '#066F71',
+        borderColor: 'rgb(255, 99, 132)',
+        data: viewsArr
+      }]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+}
+
 
